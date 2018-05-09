883dbd3c5f000dd1ab4674ebd9327974c51c4cde
Dan <dan@designmynight.com>
2018-05-09 22:21:17 +0100

    feat: added intent for specifying number of players

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@
  */
 import NewGameRequestHandler from './handlers/NewGameRequestHandler'
 import UnknownCommandRequestHandler from './handlers/UnknownCommandRequestHandler'
+import SpecifyNumberOfPlayersRequestHandler from './handlers/SpecifyNumberOfPlayersRequestHandler'
+
 import { SkillBuilders } from 'ask-sdk-core'
 import { RequestEnvelope } from 'ask-sdk-model'
 
@@ -10,6 +12,7 @@ exports.handler = SkillBuilders
     .custom()
     .addRequestHandlers(
         new NewGameRequestHandler,
+        new SpecifyNumberOfPlayersRequestHandler,
         new UnknownCommandRequestHandler
     )
     .lambda()
