830a41ecb5a21d3288f1f96ad4616f492f322301
Dan <dan@designmynight.com>
2018-05-12 22:36:03 +0100

    refact: moved imports of Cards to their own file

--- models/Card.ts
+++ models/Card.ts
@@ -1,9 +1,334 @@
 import Player from './Player'
 
-export default interface Card {
+export enum Suite {
+    Spades = 'Spades',
+    Clubs = 'Clubs',
+    Hearts = 'Hearts',
+    Diamonds = 'Diamonds'
+}
+
+interface CardInterface {
     name: string
-    isPicked: boolean
-    isSpecial: boolean
-    pickedBy: Player | null
+    suite: Suite
     descriptions: string[]
+    isSpecial: boolean
+    isPicked: boolean
+    pickedBy: Player | null
+}
+
+export abstract class Card {
+    isPicked = false
+    pickedBy = null
+}
+
+export abstract class AceCard extends Card {
+    name = 'Ace'
+    isSpecial = false
+    descriptions = [
+        'Waterfall.'
+    ]
+}
+
+export abstract class TwoCard extends Card {
+    name = 'Two'
+    isSpecial = false
+    descriptions = [
+        'Two is choose.'
+    ]
+}
+
+export abstract class ThreeCard extends Card {
+    name = 'Three'
+    isSpecial = false
+    descriptions = [
+        'Three is me.'
+    ]
+}
+
+export abstract class FourCard extends Card {
+    name = 'Four'
+    isSpecial = false
+    descriptions = [
+        'This is for the ladies.'
+    ]
+}
+
+export abstract class FiveCard extends Card {
+    name = 'Five'
+    isSpecial = true
+    descriptions = [
+        'Thumbmaster'
+    ]
+}
+
+export abstract class SixCard extends Card {
+    name = 'Six'
+    isSpecial = false
+    descriptions = [
+        'This is for the blokes.'
+    ]
+}
+
+export abstract class SevenCard extends Card {
+    name = 'Seven'
+    isSpecial = true
+    descriptions = [
+        'Heaven'
+    ]
+}
+
+export abstract class EightCard extends Card {
+    name = 'Eight'
+    isSpecial = false
+    descriptions = [
+        'Eight is mate'
+    ]
+}
+
+export abstract class NineCard extends Card {
+    name = 'Nine'
+    isSpecial = false
+    descriptions = [
+        'Nine is rhyme'
+    ]
+}
+
+export abstract class TenCard extends Card {
+    name = 'Ten'
+    isSpecial = false
+    descriptions = [
+        'Categories'
+    ]
+}
+
+export abstract class JackCard extends Card {
+    name = 'Jack'
+    isSpecial = true
+    descriptions = [
+        'Make a rule'
+    ]
+}
+
+export abstract class QueenCard extends Card {
+    name = 'Queen'
+    isSpecial = true
+    descriptions = [
+        'Question master'
+    ]
+}
+
+export abstract class KingCard extends Card {
+    name = 'King'
+    isSpecial = false
+    descriptions = [
+        'Pour some drink in the centre glass'
+    ]
+}
+
+export class AceSpades extends AceCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class TwoSpades extends TwoCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class ThreeSpades extends ThreeCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class FourSpades extends FourCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class FiveSpades extends FiveCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class SixSpades extends SixCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class SevenSpades extends SevenCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class EightSpades extends EightCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class NineSpades extends NineCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class TenSpades extends TenCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class JackSpades extends JackCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class QueenSpades extends QueenCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class KingSpades extends KingCard implements CardInterface {
+    suite = Suite.Spades
+}
+
+export class AceClubs extends AceCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class TwoClubs extends TwoCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class ThreeClubs extends ThreeCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class FourClubs extends FourCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class FiveClubs extends FiveCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class SixClubs extends SixCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class SevenClubs extends SevenCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class EightClubs extends EightCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class NineClubs extends NineCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class TenClubs extends TenCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class JackClubs extends JackCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class QueenClubs extends QueenCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class KingClubs extends KingCard implements CardInterface {
+    suite = Suite.Clubs
+}
+
+export class AceHearts extends AceCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class TwoHearts extends TwoCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class ThreeHearts extends ThreeCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class FourHearts extends FourCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class FiveHearts extends FiveCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class SixHearts extends SixCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class SevenHearts extends SevenCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class EightHearts extends EightCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class NineHearts extends NineCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class TenHearts extends TenCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class JackHearts extends JackCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class QueenHearts extends QueenCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class KingHearts extends KingCard implements CardInterface {
+    suite = Suite.Hearts
+}
+
+export class AceDiamonds extends AceCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class TwoDiamonds extends TwoCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class ThreeDiamonds extends ThreeCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class FourDiamonds extends FourCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class FiveDiamonds extends FiveCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class SixDiamonds extends SixCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class SevenDiamonds extends SevenCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class EightDiamonds extends EightCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class NineDiamonds extends NineCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class TenDiamonds extends TenCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class JackDiamonds extends JackCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class QueenDiamonds extends QueenCard implements CardInterface {
+    suite = Suite.Diamonds
+}
+
+export class KingDiamonds extends KingCard implements CardInterface {
+    suite = Suite.Diamonds
 }
--- services/DeckService.ts
+++ services/DeckService.ts
@@ -1,354 +1,40 @@
-import Card from '../models/Card'
+import * as Cards from '../models/Card'
 import Player from '../models/Player';
 
-enum Suite {
-    Spades = 'Spades',
-    Clubs = 'Clubs',
-    Hearts = 'Hearts',
-    Diamonds = 'Diamonds'
-}
-
-interface CardInterface {
-    name: string
-    suite: Suite
-    descriptions: string[]
-    isSpecial: boolean
-}
-
-abstract class Card {
-    isSpecial: boolean = false
-}
-
-export abstract class AceCard extends Card {
-    name = 'Ace'
-    isSpecial = false
-    descriptions = [
-        'Waterfall.'
-    ]
-}
-
-export abstract class TwoCard extends Card {
-    name = 'Two'
-    isSpecial = false
-    descriptions = [
-        'Two is choose.'
-    ]
-}
-
-export abstract class ThreeCard extends Card {
-    name = 'Three'
-    isSpecial = false
-    descriptions = [
-        'Three is me.'
-    ]
-}
-
-export abstract class FourCard extends Card {
-    name = 'Four'
-    isSpecial = false
-    descriptions = [
-        'This is for the ladies.'
-    ]
-}
-
-export abstract class FiveCard extends Card {
-    name = 'Five'
-    isSpecial = true
-    descriptions = [
-        'Thumbmaster'
-    ]
-}
-
-export abstract class SixCard extends Card {
-    name = 'Six'
-    isSpecial = false
-    descriptions = [
-        'This is for the blokes.'
-    ]
-}
-
-export abstract class SevenCard extends Card {
-    name = 'Seven'
-    isSpecial = true
-    descriptions = [
-        'Heaven'
-    ]
-}
-
-export abstract class EightCard extends Card {
-    name = 'Eight'
-    isSpecial = false
-    descriptions = [
-        'Eight is mate'
-    ]
-}
-
-export abstract class NineCard extends Card {
-    name = 'Nine'
-    isSpecial = false
-    descriptions = [
-        'Nine is rhyme'
-    ]
-}
-
-export abstract class TenCard extends Card {
-    name = 'Ten'
-    isSpecial = false
-    descriptions = [
-        'Categories'
-    ]
-}
-
-export abstract class JackCard extends Card {
-    name = 'Jack'
-    isSpecial = true
-    descriptions = [
-        'Make a rule'
-    ]
-}
-
-export abstract class QueenCard extends Card {
-    name = 'Queen'
-    isSpecial = true
-    descriptions = [
-        'Question master'
-    ]
-}
-
-export abstract class KingCard extends Card {
-    name = 'King'
-    isSpecial = false
-    descriptions = [
-        'Pour some drink in the centre glass'
-    ]
-}
-
-class AceSpades extends AceCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class TwoSpades extends TwoCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class ThreeSpades extends ThreeCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class FourSpades extends FourCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class FiveSpades extends FiveCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class SixSpades extends SixCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class SevenSpades extends SevenCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class EightSpades extends EightCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class NineSpades extends NineCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class TenSpades extends TenCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class JackSpades extends JackCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class QueenSpades extends QueenCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class KingSpades extends KingCard implements CardInterface {
-    suite = Suite.Spades
-}
-
-class AceClubs extends AceCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class TwoClubs extends TwoCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class ThreeClubs extends ThreeCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class FourClubs extends FourCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class FiveClubs extends FiveCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class SixClubs extends SixCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class SevenClubs extends SevenCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class EightClubs extends EightCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class NineClubs extends NineCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class TenClubs extends TenCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class JackClubs extends JackCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class QueenClubs extends QueenCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class KingClubs extends KingCard implements CardInterface {
-    suite = Suite.Clubs
-}
-
-class AceHearts extends AceCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class TwoHearts extends TwoCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class ThreeHearts extends ThreeCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class FourHearts extends FourCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class FiveHearts extends FiveCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class SixHearts extends SixCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class SevenHearts extends SevenCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class EightHearts extends EightCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class NineHearts extends NineCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class TenHearts extends TenCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class JackHearts extends JackCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class QueenHearts extends QueenCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class KingHearts extends KingCard implements CardInterface {
-    suite = Suite.Hearts
-}
-
-class AceDiamonds extends AceCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class TwoDiamonds extends TwoCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class ThreeDiamonds extends ThreeCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class FourDiamonds extends FourCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class FiveDiamonds extends FiveCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class SixDiamonds extends SixCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class SevenDiamonds extends SevenCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class EightDiamonds extends EightCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class NineDiamonds extends NineCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class TenDiamonds extends TenCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class JackDiamonds extends JackCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class QueenDiamonds extends QueenCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
-class KingDiamonds extends KingCard implements CardInterface {
-    suite = Suite.Diamonds
-}
-
 export default class DeckService {
-    private cards: Card[]
+    cards: Cards.Card[]
 
     /**
      * Gets a random unpicked card from the deck and marks it
      * as picked
      * @return {Card}
      */
-    getRandomCard (): Card {
+    getNewCardForPlayer (player: Player): Cards.Card {
+        const remainingCards = this.getRemainingCards()
+        const randomIndex = Math.floor(Math.random() * remainingCards.length)
+        const newCard = remainingCards[randomIndex]
 
+        // Now get that card from the deck.
+        const newCardFromDeck = this.cards.find(card => card === newCard)
+
+        newCardFromDeck.isPicked = true
+        newCardFromDeck.pickedBy = player
+
+        return newCardFromDeck
     }
 
     /**
      * Gets all the cards that haven't been picked yet
      * @return {Card[]}
      */
-    getRemainingCards (): Card[] {
+    getRemainingCards (): Cards.Card[] {
         return this.cards.filter(card => !card.isPicked)
+    }
+
+    getRemainingCardsOfType (card: Cards.Card): Cards.Card[] {
+        return this.getRemainingCards().filter((remaining) => {
+            return remaining === card
+        })
     }
 
     /**
@@ -375,7 +61,7 @@ export default class DeckService {
      * @param {Player} player
      * @return {Card[]}
      */
-    getAllCardsPickedBy (player: Player): Card[] {
+    getAllCardsPickedBy (player: Player): Cards.Card[] {
         return this.cards.filter(card => card.pickedBy === player)
     }
 
@@ -383,64 +69,68 @@ export default class DeckService {
 
     // }
 
-    static generateNewDeck (): Card[] {
+    static generateNewDeck (): Cards.Card[] {
         return [
-            new AceSpades,
-            new TwoSpades,
-            new ThreeSpades,
-            new FourSpades,
-            new FiveSpades,
-            new SixSpades,
-            new SevenSpades,
-            new EightSpades,
-            new NineSpades,
-            new TenSpades,
-            new JackSpades,
-            new QueenSpades,
-            new KingSpades,
-            new AceClubs,
-            new TwoClubs,
-            new ThreeClubs,
-            new FourClubs,
-            new FiveClubs,
-            new SixClubs,
-            new SevenClubs,
-            new EightClubs,
-            new NineClubs,
-            new TenClubs,
-            new JackClubs,
-            new QueenClubs,
-            new KingClubs,
-            new AceHearts,
-            new TwoHearts,
-            new ThreeHearts,
-            new FourHearts,
-            new FiveHearts,
-            new SixHearts,
-            new SevenHearts,
-            new EightHearts,
-            new NineHearts,
-            new TenHearts,
-            new JackHearts,
-            new QueenHearts,
-            new KingHearts,
-            new AceDiamonds,
-            new TwoDiamonds,
-            new ThreeDiamonds,
-            new FourDiamonds,
-            new FiveDiamonds,
-            new SixDiamonds,
-            new SevenDiamonds,
-            new EightDiamonds,
-            new NineDiamonds,
-            new TenDiamonds,
-            new JackDiamonds,
-            new QueenDiamonds,
-            new KingDiamonds
+            new Cards.AceSpades,
+            new Cards.TwoSpades,
+            new Cards.ThreeSpades,
+            new Cards.FourSpades,
+            new Cards.FiveSpades,
+            new Cards.SixSpades,
+            new Cards.SevenSpades,
+            new Cards.EightSpades,
+            new Cards.NineSpades,
+            new Cards.TenSpades,
+            new Cards.JackSpades,
+            new Cards.QueenSpades,
+            new Cards.KingSpades,
+            new Cards.AceClubs,
+            new Cards.TwoClubs,
+            new Cards.ThreeClubs,
+            new Cards.FourClubs,
+            new Cards.FiveClubs,
+            new Cards.SixClubs,
+            new Cards.SevenClubs,
+            new Cards.EightClubs,
+            new Cards.NineClubs,
+            new Cards.TenClubs,
+            new Cards.JackClubs,
+            new Cards.QueenClubs,
+            new Cards.KingClubs,
+            new Cards.AceHearts,
+            new Cards.TwoHearts,
+            new Cards.ThreeHearts,
+            new Cards.FourHearts,
+            new Cards.FiveHearts,
+            new Cards.SixHearts,
+            new Cards.SevenHearts,
+            new Cards.EightHearts,
+            new Cards.NineHearts,
+            new Cards.TenHearts,
+            new Cards.JackHearts,
+            new Cards.QueenHearts,
+            new Cards.KingHearts,
+            new Cards.AceDiamonds,
+            new Cards.TwoDiamonds,
+            new Cards.ThreeDiamonds,
+            new Cards.FourDiamonds,
+            new Cards.FiveDiamonds,
+            new Cards.SixDiamonds,
+            new Cards.SevenDiamonds,
+            new Cards.EightDiamonds,
+            new Cards.NineDiamonds,
+            new Cards.TenDiamonds,
+            new Cards.JackDiamonds,
+            new Cards.QueenDiamonds,
+            new Cards.KingDiamonds
         ]
     }
 
-    constructor (cards: Card[]) {
-        this.cards = cards
+    constructor (sessionData: any) {
+        const newDeck = DeckService.generateNewDeck()
+
+        // Apply the session data to the new deck.
+
+        this.cards = newDeck
     }
 }
