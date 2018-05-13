88d1c485ec56abc9e161c95c68ab12fb051b7ea2
Dan <dan@designmynight.com>
2018-05-13 01:22:50 +0100

    feat: added a handler for specifying player names

---
+++ handlers/SpecifyPlayerRequestHandler.ts
@@ -0,0 +1,46 @@
+import RingOfFireService from '../services/RingOfFireService'
+import * as Alexa from 'ask-sdk'
+import { Response, IntentRequest } from 'ask-sdk-model'
+
+export default class SpecifyPlayerRequestHandler implements Alexa.RequestHandler {
+    private intentName = 'SpecifyPlayer'
+
+    name: string
+    game: RingOfFireService
+
+    canHandle (handlerInput): boolean {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === this.intentName
+    }
+
+    handle (handlerInput: Alexa.HandlerInput): Promise<Response> | Response {
+        this.game = new RingOfFireService(handlerInput.attributesManager, handlerInput.requestEnvelope)
+        const request: IntentRequest = <IntentRequest> handlerInput.requestEnvelope.request
+        const name = request.intent.slots.name.value
+
+        this.name = name
+
+        this.game.setupService.specifyNameOfNextPlayer(name)
+
+        if (this.game.setupService.haveAllPlayersBeenDefined()) {
+            this.game.persistPlayersFromSetup()
+
+            return handlerInput.responseBuilder
+                .speak('Hey ' + this.name + '! Looks like that\'s everyone. When you\'re ready for a card, just say "Alexa, ask Ring of Fire for a card')
+                .getResponse()
+        }
+
+        return handlerInput.responseBuilder
+            .speak(this.getResponseSpeech())
+            .reprompt(this.getRepromptSpeech())
+            .getResponse()
+    }
+
+    getResponseSpeech () {
+        return 'Hey ' + this.name + '! ' + this.getRepromptSpeech()
+    }
+
+    getRepromptSpeech () {
+        return 'Player ' + (this.game.setupService.playerNames.length + 1) + ', what\'s your name?'
+    }
+}
